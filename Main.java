6bbcbc4b24e9cfcaf15117fe9d82fddb6949cd3f
mosafari <m.safari2680@gmail.com>
2023-08-08 18:14:45 +0330

    Variables

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+  public static void main(String[] args) {
+    String name = "John";
+    System.out.println(name);
+    int myNum = 15;
+    System.out.println(myNum);
+    int myNum2;
+    myNum2 = 15;
+    System.out.println(myNum2);
+    myNum = 20;  // myNum is now 20
+    System.out.println(myNum);
+    // final int myNum = 15;
+    // myNum = 20;  // will generate an error: cannot assign a value to a final variable
+    // Other Types
+    // float myFloatNum = 5.99f;
+    // char myLetter = 'D';
+    // boolean myBool = true;
+    // String myText = "Hello";
+  }
+}
