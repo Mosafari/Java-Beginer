bf02df6dc0cc52183e0c9f6407234b5d93db827b
mosafari <m.safari2680@gmail.com>
2023-08-08 18:05:23 +0330

    println str,num

--- Helloworld.java
+++ Helloworld.java
@@ -3,7 +3,10 @@
 
 public class Helloworld {
   public static void main(String[] args) {
+    System.out.print("Hello World "); // without new line
     System.out.println("Hello World");
+    System.out.println(50000);
+    System.out.println(2 * 5);
   }
 }
 
