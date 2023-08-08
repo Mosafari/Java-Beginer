cb3e6a1ec816b3459878beb411a41738e70f321e
mosafari <m.safari2680@gmail.com>
2023-08-08 21:35:52 +0330

    Concatenation

--- jstring.java
+++ jstring.java
@@ -12,6 +12,13 @@ public class jstring {
     String txt3 = "Please locate where 'locate' occurs!";
     System.out.println(txt3.indexOf("locate")); // Outputs 7
 
-    
+    // The + operator can be used between strings to combine them. This is called concatenation:
+    String firstName = "John";
+    String lastName = "Doe";
+    System.out.println(firstName + " " + lastName);
+
+    // You can also use the concat() method to concatenate two strings:
+    System.out.println(firstName.concat(lastName));
+
   }
 }
