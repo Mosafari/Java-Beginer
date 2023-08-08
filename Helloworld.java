129acf6e7ee4137d0659a2a117dce3fc337f4585
mosafari <m.safari2680@gmail.com>
2023-08-08 17:53:09 +0330

    hello world

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+// In Java, every application begins with a class 
+// name, and that class must match the filename.
+
+public class Helloworld {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+// to compile : javac Name.java 
+// to run : java Name.java 
