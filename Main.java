8d3bec5e15c94f5918a37de8a4b4576f64472e07
mosafari <m.safari2680@gmail.com>
2023-08-08 18:50:15 +0330

    Characters

--- Main.java
+++ Main.java
@@ -64,6 +64,18 @@ public class Main {
     System.out.println(isJavaFun);     // Outputs true
     System.out.println(isFishTasty);   // Outputs false
 
+    // char
+    char myGrade = 'B';
+    System.out.println(myGrade);
+    // Alternatively, if you are familiar with ASCII values, you can use those to display certain characters:
+    char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+    System.out.println(myVar1);
+    System.out.println(myVar2);
+    System.out.println(myVar3);
+
+    // The String data type is used to store a sequence of characters (text). String values must be surrounded by double quotes
+    String greeting = "Hello World";
+    System.out.println(greeting);
 
 
   }
