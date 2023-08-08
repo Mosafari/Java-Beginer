312c0215b38afe51b8b9c3f68c4732b80d20b4f5
mosafari <m.safari2680@gmail.com>
2023-08-08 22:04:22 +0330

    Java math

---
+++ jmath.java
@@ -0,0 +1,8 @@
+public class jmath {
+  public static void main(String[] args) {
+    // The Java Math class has many methods that allows you to perform mathematical tasks on numbers.
+    System.out.println(Math.max(5, 10)); // find the highest value of x and y
+    System.out.println(Math.min(5, 10)); // find the lowest value of x and y
+    System.out.println(Math.sqrt(64)); // returns the square root of x
+  }
+}
