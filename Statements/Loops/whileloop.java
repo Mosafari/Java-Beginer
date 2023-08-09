1a9d72e7869d19fa981102ad39666f8abf0d6a1d
mosafari <m.safari2680@gmail.com>
2023-08-09 19:28:31 +0330

    while statement

---
+++ Statements/Loops/whileloop.java
@@ -0,0 +1,15 @@
+package Statements.Loops;
+
+public class whileloop {
+    public static void main(String[] args) {
+        // Loops can execute a block of code as long as a specified condition is reached.
+        // Loops are handy because they save time, reduce errors, and they make code more readable.
+        int i = 0;
+        while (i < 5) {
+        System.out.println(i);
+        i++;
+        }
+        // Note: Do not forget to increase the variable used in the condition, otherwise the loop will never end!
+
+    }
+}
