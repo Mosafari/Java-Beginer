6c761bcb11bfbcaefbf6b7845abf23b8b280c5ad
mosafari <m.safari2680@gmail.com>
2023-08-10 12:41:41 +0330

    continue & break

---
+++ Statements/Loops/continuest.java
@@ -0,0 +1,20 @@
+package Statements.Loops;
+
+public class continuest {
+    public static void main(String[] args) {
+        //  Break
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+              break;
+            }
+            System.out.println(i);
+          }
+        //   Continue
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+              continue;
+            }
+            System.out.println(i);
+          }
+    }
+}
