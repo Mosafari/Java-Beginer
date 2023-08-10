56452e275c56e434e960f91a4afdfcb908af94d9
mosafari <m.safari2680@gmail.com>
2023-08-10 12:34:40 +0330

    for loop

---
+++ Statements/Loops/forloop.java
@@ -0,0 +1,17 @@
+package Statements.Loops;
+
+public class forloop {
+    public static void main(String[] args) {
+        // When you know exactly how many times you want to loop through a block of code, use the for loop instead of a while loop
+        // Statement 1 is executed (one time) before the execution of the code block.
+        // Statement 2 defines the condition for executing the code block.
+        // Statement 3 is executed (every time) after the code block has been executed.
+        // The example below will print the numbers 0 to 4:
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+          }
+              
+        
+    }
+    
+}
