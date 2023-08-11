a4c7675946b9d6cec7773ec3bc1e94cd1d948c67
mosafari <m.safari2680@gmail.com>
2023-08-11 19:58:43 +0330

    Multidimensional Arrays

--- Statements/arrayj.java
+++ Statements/arrayj.java
@@ -30,5 +30,21 @@ public class arrayj {
         for (String i : cars) {
             System.out.println(i);
           }
+
+        //   A multidimensional array is an array of arrays.
+        //   Multidimensional arrays are useful when you want to store data as a tabular form, like a table with rows and columns.
+        //   To create a two-dimensional array, add each array within its own set of curly braces:
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        System.out.println(myNumbers[1][2]); // Outputs 7
+
+        // You can also change the value of an element:
+        myNumbers[1][2] = 9;
+        System.out.println(myNumbers[1][2]); // Outputs 9 instead of 7
+        // We can also use a for loop inside another for loop to get the elements of a two-dimensional array (we still have to point to the two indexes)
+        for (int i = 0; i < myNumbers.length; ++i) {
+            for(int j = 0; j < myNumbers[i].length; ++j) {
+              System.out.println(myNumbers[i][j]);
+            }
+        }
     }
 }
