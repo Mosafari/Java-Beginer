cc4e80729f9b06e279d01dd733dbd8b101b182ba
mosafari <m.safari2680@gmail.com>
2023-08-13 15:07:35 +0330

    Java Scope

--- Statements/methods.java
+++ Statements/methods.java
@@ -70,6 +70,31 @@ public class methods {
         System.out.println("int: " + myNum1);
         System.out.println("double: " + myNum2);
         // Note: Multiple methods can have the same name as long as the number and/or type of parameters are different.
+        
+        // In Java, variables are only accessible inside the region they are created. This is called scope.
+        // If we try to access a local variable outside its scope, we will get this error message: 
+        // Exception in thread "main" java.lang.ExceptionInInitializerError
+
+        // ** Variables declared directly inside a method are available anywhere in the method following the line of code in which they were declared.
+        // A block of code refers to all of the code between curly braces {}.
+        // Variables declared inside blocks of code are only accessible by the code between the curly braces, which follows the line in which the variable was declared
+        // Code here CANNOT use x
+
+        { // This is a block
+
+            // Code here CANNOT use x
+    
+            int x = 100;
+    
+            // Code here CAN use x
+            System.out.println(x);
+    
+        } // The block ends here
+    
+        // Code here CANNOT use x
+
+        // A block of code may exist on its own or it can belong to an if, while or for statement. In the case of for statements, variables declared in the statement 
+        // itself are also available inside the block's scope.
     }
     
 }
