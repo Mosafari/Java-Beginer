c3eb7f64fde51902f845f46312f2bd3974243ca9
mosafari <m.safari2680@gmail.com>
2023-08-14 18:44:10 +0330

    Java Class Methods

--- OOP/Main.java
+++ OOP/Main.java
@@ -1,6 +1,11 @@
 package OOP;
 
+import Statements.methods;
+
 public class Main {
+    static void myMethod() {
+        System.out.println("Hello World!");
+      }
     int x = 5;
     // You can also create an object of a class and access it in another class.
     //  This is often used for better organization of classes (one class has all the attributes and
@@ -15,6 +20,15 @@ public class Main {
 
     int z;
     final int s = 5;
+    // Static method
+    static void myStaticMethod() {
+        System.out.println("Static methods can be called without creating objects");
+    }
+
+    // Public method
+    public void myPublicMethod() {
+        System.out.println("Public methods must be called by creating objects");
+    }
     public static void main(String[] args) {
         Main obj1 = new Main();
         System.out.println(obj1.x); // 5
@@ -40,6 +54,27 @@ public class Main {
         System.out.println(obj1.x); // 25
 
         // also You can specify as many attributes as you want
+
+        // Class Methods
+        // methods are declared within a class, and that they are used to perform certain actions
+        //  To call a method, write the method's name followed by two parentheses () and a semicolon;
+        myMethod();
+
+        // Static vs. Public
+        // You will often see Java programs that have either static or public attributes and methods.
+        // In the example above, we created a static method, which means that it can be accessed without 
+        // creating an object of the class, unlike public, which can only be accessed by objects
+        
+        myStaticMethod();
+        obj1.myPublicMethod();
+
+        
+        // Remember that..
+        // The dot (.) is used to access the object's attributes and methods.
+        // To call a method in Java, write the method name followed by a set of parentheses (), 
+        // followed by a semicolon (;).
+        // A class must have a matching filename (Main and Main.java).
+
 
     }
 
--- OOP/Second.java
+++ OOP/Second.java
@@ -4,5 +4,7 @@ public class Second {
     public static void main(String[] args) {
         Main myObj = new Main();
         System.out.println(myObj.x);
+        myObj.myPublicMethod(); // public method from Main
+        Main.myStaticMethod(); // Static method from Main
     }
 }
