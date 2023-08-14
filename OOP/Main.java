abf89a70c22af455ba9f2001c723995aecfd102d
mosafari <m.safari2680@gmail.com>
2023-08-14 18:18:36 +0330

    Using Multiple Classes

---
+++ OOP/Main.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class Main {
+    int x = 5;
+    // You can also create an object of a class and access it in another class.
+    //  This is often used for better organization of classes (one class has all the attributes and
+    //   methods, while the other class holds the main() method (code to be executed)).
+    // Remember that the name of the java file should match the class name.
+
+
+}
---
+++ OOP/Second.java
@@ -0,0 +1,8 @@
+package OOP;
+
+public class Second {
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        System.out.println(myObj.x);
+    }
+}
